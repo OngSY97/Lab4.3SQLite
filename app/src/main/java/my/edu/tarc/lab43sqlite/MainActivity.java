17137fe85f3dbdb7572c95a73b7fff70777bf051
OngSY97 <sheakyer97@hotmail.com>
2017-12-21 13:59:58 +0800

    initial

--- app/src/main/java/my/edu/tarc/lab43sqlite/MainActivity.java
+++ app/src/main/java/my/edu/tarc/lab43sqlite/MainActivity.java
@@ -1,15 +1,26 @@
 package my.edu.tarc.lab43sqlite;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import my.edu.tarc.lab43sqlite.Model.UserRecord;
+import my.edu.tarc.lab43sqlite.Model.UserRecordAdapter;
+import my.edu.tarc.lab43sqlite.Model.UserSQLHelper;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListView listViewRecords;
+    UserSQLHelper userSQLHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +29,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        listViewRecords = (ListView)findViewById(R.id.listViewRecords);
+        listViewRecords.setOnItemClickListener(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabAdd);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -49,4 +63,32 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), "Position: " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateList();
+    }
+
+    private void updateList() {
+        //Retrieve records from SQLite
+        userSQLHelper = new UserSQLHelper(this);
+
+        final List<UserRecord> values = userSQLHelper.getAllUsers();
+
+        if(values.isEmpty()){
+            Toast.makeText(getApplicationContext(), getString(R.string.no_record_message), Toast.LENGTH_SHORT).show();
+        }
+
+        UserRecordAdapter adapter = new UserRecordAdapter(this, R.layout.user_record, values);
+
+        //Link adapter to ListView
+        listViewRecords.setAdapter(adapter);
+    }
+
 }
